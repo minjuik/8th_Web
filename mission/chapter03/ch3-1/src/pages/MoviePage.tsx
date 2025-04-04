52ab91f1fda9fe7651b4f415f9e5805a9bf913cd
minjuik <140732219+minjuik@users.noreply.github.com>
2025-04-05 03:42:43 +0900

    mission:3주차 미션1 완료

--- mission/chapter03/ch3-1/src/pages/MoviePage.tsx
+++ mission/chapter03/ch3-1/src/pages/MoviePage.tsx
@@ -10,7 +10,7 @@ export default function MoviePage() {
     useEffect((): void=> {
         const fetchMovies = async(): Promise<void> => {
             const {data} = await axios.get<MovieResponse>(
-                `https://api.themoviedb.org/3/movie/popular?language=en-US&page=2`, 
+                `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`, 
                 {
                     headers: {
                         Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
@@ -25,7 +25,7 @@ export default function MoviePage() {
     }, [])
 
     return (
-        // div ClassName: 화면 크기에 따라 몇개씩 배치될 것인지 정할 수 잇다다
+        // div ClassName: 화면 크기에 따라 몇개씩 배치될 것인지 정할 수 잇다
         <div className="p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 
         lg:grid-cols-5 xl:grid-cols-6">
             {movies.map((movie) => (
