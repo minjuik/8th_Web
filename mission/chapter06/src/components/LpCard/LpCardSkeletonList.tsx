e62716c4ecae1366f9503ccaa118736812591c70
minjuik <140732219+minjuik@users.noreply.github.com>
2025-05-10 03:21:12 +0900

    6주차 미션2

---
+++ mission/chapter06/src/components/LpCard/LpCardSkeletonList.tsx
@@ -0,0 +1,15 @@
+import { LpCardSkeleton } from "./LpCardSkeleton";
+
+interface LpCardSkeletonListProps {
+  count: number;
+}
+
+export const LpCardSkeletonList = ({ count }: LpCardSkeletonListProps) => {
+  return (
+    <>
+      {new Array(count).fill(0).map((_, idx) => (
+        <LpCardSkeleton key={idx} />
+      ))}
+    </>
+  );
+};
--- mission/chapter06/src/pages/HomePage.tsx
+++ mission/chapter06/src/pages/HomePage.tsx
@@ -1,27 +1,60 @@
-import { useState } from "react";
-import useGetLpList from "../hooks/queries/useGetLpList";
+import { useEffect, useState } from "react";
+import useGetInfiniteLpList from "../hooks/queries/useGetInfiniteLpList";
+import { PAGINATION_ORDER } from "../enums/common";
+import { useInView } from "react-intersection-observer";
+import { LpCard } from "../components/LpCard/LpCard";
+import { LpCardSkeletonList } from "../components/LpCard/LpCardSkeletonList";
+import { LpCardSkeleton } from "../components/LpCard/LpCardSkeleton";
 
 const HomePage = () => {
   const [search, setSearch] = useState("");
-  const { data, isPending, isError } = useGetLpList({
-    search,
-    limit: 30,
+  //   const { data, isPending, isError } = useGetLpList({
+  //     search,
+  //     limit: 30,
+  //   });
+  const {
+    data: lps,
+    isFetching,
+    hasNextPage,
+    isPending,
+    fetchNextPage,
+    isError,
+  } = useGetInfiniteLpList(10, search, PAGINATION_ORDER.desc);
+
+  // ref -> 특정한 HTML 요소를 감시할 수 있다
+  // inView -> 그 요소가 화면에 보이면 true
+  const { ref, inView } = useInView({
+    threshold: 0,
   });
 
-  if (isPending) {
-    return <div className="mt-20">Loading...</div>;
-  }
-    
+  useEffect(() => {
+    if (inView) {
+      !isFetching && hasNextPage && fetchNextPage();
+    }
+  }, [inView, isFetching, hasNextPage, fetchNextPage]);
+
+//   if (isPending) {
+//     return <div className="mt-20">Loading...</div>;
+//   }
+
   if (isError) {
     return <div className="mt-20">Error!</div>;
   }
-    
+
   return (
-    <div className="mt-10">
+    <div className="container mx-auto px-4 py-6">
       <input value={search} onChange={(e) => setSearch(e.target.value)} />
-      {data?.map((lp) => (
-        <h1>{lp.title}</h1>
-      ))}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {isPending && <LpCardSkeletonList count={20} />}
+        {lps?.pages
+          ?.map((page) => page.data.data)
+          ?.flat()
+          ?.map((lp) => (
+            <LpCard key={lp.id} lp={lp} />
+          ))}
+        {isFetching && <LpCardSkeletonList count={20} />}
+      </div>
+      <div ref={ref} className="h-2"></div>
     </div>
   );
 };
